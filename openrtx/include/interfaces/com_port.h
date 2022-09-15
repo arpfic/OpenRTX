72ce55c69d8c852d36362fef8c9d142e994d7948
Silvano Seva <silseva@fastwebnet.it>
2022-09-15 21:36:59 +0200

    Created common interface header file for serial COM port

--- openrtx/include/interfaces/com_port.h
+++ openrtx/include/interfaces/com_port.h
@@ -15,8 +15,8 @@
  *   along with this program; if not, see <http://www.gnu.org/licenses/>   *
  ***************************************************************************/
 
-#ifndef USB_VCOM_H
-#define USB_VCOM_H
+#ifndef COMPORT_H
+#define COMPORT_H
 
 #include <stdint.h>
 #include <unistd.h>
@@ -26,31 +26,38 @@ extern "C" {
 #endif
 
 /**
- * Initialise USB virtual com port. Parameters: 115200 baud, 8N1.
- * @return zero on success, negative value on failure.
+ * Initialise the serial communication port. Parameters: 115200 baud, 8N1.
  */
-void vcom_init();
+void com_init();
 
 /**
-* Write a block of data. This function blocks until all data have been sent.
-* \param buffer buffer where take data to write.
-* \param size buffer size
-* \return number of bytes written or a negative number on failure.
-*/
-ssize_t vcom_writeBlock(const void *buf, size_t len);
+ * Shut down the serial communication port.
+ */
+void com_terminate();
 
 /**
-* Read a block of data, nonblocking function.
-* \param buffer buffer where read data will be stored.
-* \param size buffer size.
-* \return number of bytes read or a negative number on failure. Note that
-* it is normal for this function to return less character than the amount
-* asked.
-*/
-ssize_t vcom_readBlock(void *buf, size_t len);
+ * Write a block of data to the serial communication port. This function blocks
+ * until all data have been sent.
+ *
+ * \param buffer buffer where take data to write.
+ * \param size buffer size
+ * \return number of bytes written or a negative number on failure.
+ */
+ssize_t com_writeBlock(const void *buf, const size_t len);
+
+/**
+ * Read a block of data from the serial communication port, nonblocking function.
+ *
+ * \param buffer buffer where read data will be stored.
+ * \param size buffer size.
+ * \return number of bytes read or a negative number on failure. Note that
+ * it is normal for this function to return less character than the amount
+ * asked.
+ */
+ssize_t com_readBlock(void *buf, const size_t len);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* USB_VCOM_H */
+#endif /* COMPORT_H */
